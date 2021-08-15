0a27e698bd8d5a00ef1914a70eb2084969d9a5b1
Stuart-Yee <ki6fzb@gmail.com>
2021-08-14 18:43:08 -0700

    added more functionality

--- Counter/server.py
+++ Counter/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, request
 
 app = Flask(__name__)
 app.secret_key = "Njamena, Chad"
@@ -7,19 +7,34 @@ app.secret_key = "Njamena, Chad"
 def index():
     if 'visits' in session:
         session['visits'] += 1
-        print(session['visits'])
     else:
         session['visits'] = 1
-    return render_template("index.html", counter=session['visits'])
+    if 'page_loads' in session:
+        session['page_loads'] += 1
+    else:
+        session['page_loads'] = 1
+    return render_template("index.html", counter=session['visits'], loads=session['page_loads'])
 
 @app.route("/twice", methods=['POST'])
 def add_two_visits():
     session['visits'] += 1
     return redirect("/")
 
-@app.route("/clear", methods=['POST'])
-def clear_session():
+@app.route("/reset", methods=['POST'])
+def reset_visits():
     session.pop('visits')
+    return redirect("/")
+
+@app.route("/destroy_session", methods=['GET','POST'])
+def clear_session():
+    session.clear()
+    return redirect("/")
+
+@app.route("/custom_count", methods=['POST'])
+def custom_incr():
+    str_num = request.form["increment"]
+    increment = int(str_num) - 1
+    session['visits'] += increment
     return redirect("/")
 
 
